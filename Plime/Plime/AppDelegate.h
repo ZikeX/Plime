fb8dfb367e14f0a8e395de33a5a7d784250fffa6
Maskmale <Maskmale@maskmales-macbook-pro.local>
2017-03-23 00:08:16 +0800

    初始化项目

---
+++ Plime/Plime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Plime
+//
+//  Created by Maskmale on 17/3/23.
+//  Copyright © 2017年 Maskmale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
